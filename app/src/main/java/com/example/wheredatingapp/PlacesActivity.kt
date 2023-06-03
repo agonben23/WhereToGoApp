8ffafbad55fdcd1310fea0dedfd67c454fc538b8
agonben23 <agonzalezb@iesrafaelalberti.es>
2023-06-03 18:38:36 +0200

    PlacesActivity creado (por terminar)

--- app/src/main/java/com/example/wheredatingapp/PlacesActivity.kt
+++ app/src/main/java/com/example/wheredatingapp/PlacesActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -24,7 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.wheredatingapp.model.Ciudad
+import androidx.compose.ui.unit.dp
 import com.example.wheredatingapp.model.Lugar
 import com.example.wheredatingapp.model.reader.ReaderWeb
 import com.example.wheredatingapp.ui.theme.WhereDatingAppTheme
@@ -32,46 +34,16 @@ import kotlinx.coroutines.launch
 
 class PlacesActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
 
-            var listaLugares by remember { mutableStateOf(listOf<Lugar>()) }
-
             WhereDatingAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
 
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                PantallaLugares()
 
-                        var ciudad by remember {
-                            mutableStateOf("")
-                        }
-
-                        Row() {
-                            Text(text = "Busca por ciudad :")
-                            TextField(value = ciudad, onValueChange = { newCiudad ->
-                                ciudad = newCiudad
-                            })
-                            Button(onClick = {
-
-                                listaLugares = cargarLugares(ciudad = ciudad)
-
-                            }) {
-                                Text(text = "Buscar")
-                            }
-                        }
-
-                        PlacesList(listaLugares = listaLugares)
-
-                    }
-
-                }
             }
         }
 
@@ -79,49 +51,82 @@ class PlacesActivity : ComponentActivity() {
     }
 }
 
-    @Composable
-    private fun cargarLugares(ciudad: String) : List<Lugar>{
 
-        val scope = rememberCoroutineScope()
+    private suspend fun cargarLugares(ciudad: String) : List<Lugar>{
 
         var listaLugares = listOf<Lugar>()
 
-        scope.launch {
+        val newList = ReaderWeb.leerLugaresbyCiudad(ciudad)
 
-            val newList = ReaderWeb.leerLugaresbyCiudad(ciudad)
-
-            if (newList != null){
+        if (newList != null){
                 listaLugares = newList
-            }
         }
+
 
         return listaLugares
 
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PantallaLugares(){
 
-    
+    val scope = rememberCoroutineScope()
 
-}
+    var listaLugares by remember {
+        mutableStateOf(listOf<Lugar>())
+    }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background) {
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            var ciudad by remember {
+                mutableStateOf("")
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(top = 30.dp)) {
+                Column(Modifier.weight(0.5f)) {
+                    Text(text = "Buscar por ciudad")
+                }
+                Column(Modifier.weight(0.5f).padding(end = 10.dp)) {
+                    TextField(value = ciudad, onValueChange = { newCiudad ->
+                        ciudad = newCiudad
+                    }
+                    )
+                }
+                Column() {
+
+
+                    Button(onClick =
+                    {
+                        scope.launch {
+                            listaLugares = cargarLugares(ciudad)
+                        }
+
+
+                    }) {
+                        Text(text = "Buscar")
+                    }
+                }
+            }
+
+            PlacesList(listaLugares = listaLugares)
+
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PantallaLugaresPreview() {
     WhereDatingAppTheme {
-        Greeting("Android")
+        PantallaLugares()
+      }
     }
-}
 
 @Composable
 fun PlacesList(listaLugares : List<Lugar>){
